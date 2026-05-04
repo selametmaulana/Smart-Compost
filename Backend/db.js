2e84f8bd114d1c260f7403399eee011407241dd0
Selamet Maulana <selametmaulana20@gmail.com>
2026-05-04 15:00:46 +0700

    fix port for deployment

--- Backend/db.js
+++ Backend/db.js
@@ -1,17 +1,13 @@
 import mysql from 'mysql2'
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+import mysql from 'mysql2/promise'
+
+const db = await mysql.createConnection({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
 })
 
-db.connect(err => {
-  if (err) {
-    console.error('DB Error:', err)
-  } else {
-    console.log('✅ MySQL Connected')
-  }
-})
+console.log("✅ MySQL Connected")
